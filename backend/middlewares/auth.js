ff56fcd5e5bcae8c7e3928e692f8cdf2abc5bc43
Spriha Shringi <126314175+Spriha-Shringi@users.noreply.github.com>
2025-01-02 22:00:09 +0530

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,16 +4,29 @@ import ErrorHandler from "./error.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const token = req.cookies.token;
+  const authHeader = req.headers.authorization; // Get the Authorization header
+  let token;
+
+  // Extract the token from the Authorization header if present
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  } else if (req.cookies.token) {
+    // Fallback to cookies for backward compatibility
+    token = req.cookies.token;
+  }
+
   console.log("Token:", token); // Log token for debugging
+
   if (!token) {
     return next(new ErrorHandler("User not authenticated.", 400));
   }
+
+  // Verify the token
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
+
   next();
 });
-
 
 export const isAuthorized = (...roles) => {
   return (req, res, next) => {
