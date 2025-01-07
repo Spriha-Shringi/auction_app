52abf31a2ea90a5d7b1614c06081e71ba2b22116
Spriha Shringi <126314175+Spriha-Shringi@users.noreply.github.com>
2025-01-07 17:36:58 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -53,6 +53,23 @@ app.use("/api/v1/bid", bidRouter);
 app.use("/api/v1/commission", commissionRouter);
 app.use("/api/v1/superadmin", superAdminRouter);
 
+const url = `https://yourappname.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
 endedAuctionCron();
 verifyCommissionCron();
 connection();
