f39c04d7bcf554c964c31e3dd37da32b76394a40
Spriha Shringi <126314175+Spriha-Shringi@users.noreply.github.com>
2025-01-13 00:22:07 +0530

    Update commissionSlice.js

--- frontend/src/store/slices/commissionSlice.js
+++ frontend/src/store/slices/commissionSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { toast } from "react-toastify";
 
+
 const commissionSlice = createSlice({
   name: "commission",
   initialState: {
